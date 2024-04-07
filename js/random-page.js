675556993541e8fdf9ea8844b43232e62024b0e2
duncan0714 <duncan.snooks@gmail.com>
2024-04-07 13:33:54 -0700

    error page addtions

---
+++ js/random-page.js
@@ -0,0 +1,13 @@
+function randomPage() {
+  var arr = [
+    "about.html",
+    "clone-hero-charts.html",
+    "excel-workbooks.html",
+    "index.html",
+    "magazine-photoshoot.html",
+    "shipyards-park-photoshoot.html",
+    "work.html",
+  ];
+  var value = arr[Math.floor(Math.random() * arr.length)];
+  alert("Would navigate to : " + value);
+}
